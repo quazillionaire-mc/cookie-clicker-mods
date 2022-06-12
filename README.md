c1a5191b140ff8d333ffe81665ddc452e6e10d3b
Jasper Andrew <jasper.q.andrew@gmail.com>
2022-06-12 10:54:03 -0400

    Update folder structure, complete chipsPerSec mod

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# Bulk Buy 50
-Adds a 50 button to Cookie Clicker's bulk buy options
+# Quazillionaire's Cookie Clicker Mods
 
 ## Usage
-Copy the contents of `bulkbuy50-bookmarklet.js` into a browser bookmark for easy access, or just paste the contents of `bulbuy50.js` into the command prompt if you prefer.
+Copy the contents of a `*-min.js` into a browser bookmark for easy access, or just paste it into the browser's developer console, if you prefer.
 
-Also available as a mod [on the Steam workshop](https://steamcommunity.com/sharedfiles/filedetails/?id=2796239524)
+Also available as mods on the Steam workshop:
+    - [Bulk Buy 50](https://steamcommunity.com/sharedfiles/filedetails/?id=2796239524)
+    - [Chips Per Second](https://steamcommunity.com/sharedfiles/filedetails/?id=2820166963)
+    
+### Bulk Buy 50
+Adds a 50 button to Cookie Clicker's bulk buy options
+
+### Chips Per Second
+Adds a display of how many heavenly chips you are gaining per second
--- bulkBuy50/main.js
+++ bulkBuy50/main.js
@@ -1,17 +1,19 @@
-Game.registerMod("bulk-buy-50",{
-	init:function(){
+Game.registerMod("bulkBuy50", {
+	init: () => {
 		// Create the new HTML element and give it the same attributes as the other bulk buttons
-		let el = document.createElement('div');
-		el.innerHTML = "50";
-		el.className = "storePreButton storeBulkAmount";
-		el.setAttribute('id', 'storeBulk50');
+		let btn50 = document.createElement('div');
+		btn50.innerHTML = "50";
+		btn50.className = "storePreButton storeBulkAmount";
+		btn50.setAttribute('id', 'storeBulk50');
 
 		// Find the parent node and the reference node to prepend the new element to
 		let parent = document.querySelector('#storeBulk'),
 			reference = document.querySelector('#storeBulk100');
 
 		// Insert the new button
-		parent.insertBefore(el, reference);
+		parent.insertBefore(btn50, reference);
+
+		let btn1, btn10, btn100, btnAll;
 
 		// Get all the other bulk buttons
 		document.querySelectorAll('.storeBulkAmount').forEach(btn => {
@@ -20,14 +22,13 @@ Game.registerMod("bulk-buy-50",{
 			// Modify the onclick behavior of the buttons so they select and deselect properly
 			let id;
 			switch (btn.innerHTML.toLowerCase()) {
-				case 'buy': return; // We don't want to change the behavior of these
-				case 'sell': return; // <--
-				case '1': id = 2; break; // These ids are taken from the original code
-				case '10': id = 3; break;
+				case 'buy':
+				case 'sell': return; // We don't want to change the behavior of buy/sell
+				case '1': id = 2; btn1 = btn; break; // These ids are taken from the original code
+				case '10': id = 3; btn10 = btn; break;
 				case '50': id = 3.5; break; // This is the new id for the 50 button
-				case '100': id = 4; break;
-				case 'all': id = 5; break;
-				default:
+				case '100': id = 4; btn100 = btn; break;
+				case 'all': id = 5; btnAll = btn; break;
 			}
 			btn.setAttribute('onclick', `Game.storeBulkButton50(${id})`);
 		});
@@ -36,12 +37,12 @@ Game.registerMod("bulk-buy-50",{
 		Game.storeBulkButton50 = (id) => {
 			if (id === 3.5) {
 				// Make the other buttons de-selected
-				if (Game.buyBulk === 1) l('storeBulk1').classList.remove('selected');
-				if (Game.buyBulk === 10) l('storeBulk10').classList.remove('selected');
-				if (Game.buyBulk === 100) l('storeBulk100').classList.remove('selected');
-				if (Game.buyBulk === -1) l('storeBulkMax').classList.remove('selected');
+				if (Game.buyBulk === 1)   btn1.classList.remove('selected');
+				if (Game.buyBulk === 10)  btn10.classList.remove('selected');
+				if (Game.buyBulk === 100) btn100.classList.remove('selected');
+				if (Game.buyBulk === -1)  btnAll.classList.remove('selected');
 				// select the 50 button
-				l('storeBulk50').classList.add('selected');
+				btn50.classList.add('selected');
 				// Do the things that the actual code does, except with 50
 				Game.buyBulk = 50;
 				Game.storeToRefresh = 1;
@@ -50,10 +51,8 @@ Game.registerMod("bulk-buy-50",{
 				// This will only be triggered if another quantity is chosen, so just 
 				// run the normal function and deselect the 50 button
 				Game.storeBulkButton(id);
-				l('storeBulk50').classList.remove('selected');
+				btn50.classList.remove('selected');
 			}
 		};
-	},
-	save:function(){},
-	load:function(str){},
+	}
 });
