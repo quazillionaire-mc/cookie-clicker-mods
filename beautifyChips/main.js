c9f70d6548af471c23765a3c33dfb5c3a9bafac7
Jasper Andrew <jasper.q.andrew@gmail.com>
2022-06-18 18:00:58 -0400

    add a nice heavenly chip icon to beautifyChips

--- beautifyChips/main.js
+++ beautifyChips/main.js
@@ -1,8 +1,26 @@
 Game.registerMod("beautifyChips", {
 	init: () => {
+		let style = document.createElement("style");
+		// style.appendChild(document.createTextNode(""));
+		document.head.appendChild(style);
+		style.sheet.insertRule('.chipCount {' +
+			'margin-right: 21px; }');
+		style.sheet.insertRule('.chipCount::after {' +
+			'content: "";' +
+			'display: block;' +
+			'position: absolute;' +
+			'right: 2px;' +
+			'top: 8px;' +
+			'background: url(img/heavenlyMoney.png);' +
+			'width: 16px;' +
+			'height: 16px; }');
+
+		Game.ascendNumber.style.whiteSpace = 'pre';
+		
 		let logic = () => {
 			var ascendNowToGet = Math.floor(Game.HowMuchPrestige(Game.cookiesReset + Game.cookiesEarned)) - Math.floor(Game.HowMuchPrestige(Game.cookiesReset));
-			Game.ascendNumber.innerHTML = '+' + Beautify(ascendNowToGet).replace(/\.\d\d$/, '').replace(' ', '&nbsp;');
+			Game.ascendNumber.innerHTML = 
+				'<span class="chipCount">' + '+' + Beautify(ascendNowToGet).replace(/\.\d\d$/, '') + '</span>';
 		};
 
 		Game.registerHook('logic', () => {
