260b3af68dfdb92ce9bc7fcce18da3e846cc532c
Jasper Andrew <jasper.q.andrew@gmail.com>
2022-06-18 20:01:26 -0400

    Made bulkBuy50 and bulkBuyX cross-compatible

--- bulkBuy50/main.js
+++ bulkBuy50/main.js
@@ -1,58 +1,61 @@
 Game.registerMod("bulkBuy50", {
-	init: () => {
+	btn50: null,
+
+	storeBulkButtonClick: function (id) {
+		if (id === 3.5) {
+			// Make the other buttons de-selected
+			if (Game.buyBulk === 1)   l('storeBulk1').classList.remove('selected');
+			if (Game.buyBulk === 10)  l('storeBulk10').classList.remove('selected');
+			if (Game.buyBulk === 100) l('storeBulk100').classList.remove('selected');
+			if (Game.buyBulk === -1)  l('storeBulkMax').classList.remove('selected');
+			// select the 50 button
+			this.btn50.classList.add('selected');
+			// Do the things that the actual code does, except with 50
+			Game.buyBulk = 50;
+			Game.storeToRefresh = 1;
+			PlaySound('snd/tick.mp3');
+		} else {
+			// This will only be triggered if one of the normal buy buttons are clicked, so just 
+			// run the normal function and deselect the X button
+			Game.storeBulkButton(id);
+			this.btn50.classList.remove('selected');
+		}
+	},	
+
+	init: function () {
+		let MOD = this;
+
 		// Create the new HTML element and give it the same attributes as the other bulk buttons
 		let btn50 = document.createElement('div');
 		btn50.innerHTML = "50";
 		btn50.className = "storePreButton storeBulkAmount";
 		btn50.setAttribute('id', 'storeBulk50');
 
-		// Find the parent node and the reference node to prepend the new element to
-		let parent = document.querySelector('#storeBulk'),
-			reference = document.querySelector('#storeBulk100');
-
-		// Insert the new button
-		parent.insertBefore(btn50, reference);
-
 		let btn1, btn10, btn100, btnAll;
 
-		// Get all the other bulk buttons
+		// Insert the new button
+		l('storeBulk').insertBefore(btn50, l('storeBulk100'));
+
+		MOD.btn50 = btn50;
+
+		// Get all the bulk buttons
 		document.querySelectorAll('.storeBulkAmount').forEach(btn => {
 			// Modify the CSS for the buttons so they all fit again
 			btn.style.width = "46px";
 			// Modify the onclick behavior of the buttons so they select and deselect properly
 			let id;
 			switch (btn.innerHTML.toLowerCase()) {
-				case 'buy':
-				case 'sell': return; // We don't want to change the behavior of buy/sell
 				case '1': id = 2; btn1 = btn; break; // These ids are taken from the original code
 				case '10': id = 3; btn10 = btn; break;
 				case '50': id = 3.5; break; // This is the new id for the 50 button
 				case '100': id = 4; btn100 = btn; break;
 				case 'all': id = 5; btnAll = btn; break;
+				default: return;
 			}
-			btn.setAttribute('onclick', `Game.storeBulkButton50(${id})`);
+			btn.setAttribute('onclick', `Game.mods['${MOD.id}'].storeBulkButtonClick(${id})`);
 		});
 
-		// Attach a new function to the Game object
-		Game.storeBulkButton50 = (id) => {
-			if (id === 3.5) {
-				// Make the other buttons de-selected
-				if (Game.buyBulk === 1)   btn1.classList.remove('selected');
-				if (Game.buyBulk === 10)  btn10.classList.remove('selected');
-				if (Game.buyBulk === 100) btn100.classList.remove('selected');
-				if (Game.buyBulk === -1)  btnAll.classList.remove('selected');
-				// select the 50 button
-				btn50.classList.add('selected');
-				// Do the things that the actual code does, except with 50
-				Game.buyBulk = 50;
-				Game.storeToRefresh = 1;
-				PlaySound('snd/tick.mp3');
-			} else {
-				// This will only be triggered if another quantity is chosen, so just 
-				// run the normal function and deselect the 50 button
-				Game.storeBulkButton(id);
-				btn50.classList.remove('selected');
-			}
-		};
+		let bulkBuyX = Game.mods['bulkBuyX'];
+		if (bulkBuyX !== undefined) bulkBuyX.adjustForBulk50();
 	}
 });
--- bulkBuyX/main.js
+++ bulkBuyX/main.js
@@ -1,54 +1,69 @@
 Game.registerMod("bulkBuyX", {
-	// mod values
 	curVal: null,
 	btnX: null,
 
-	// custom functions
-	storeBulkButtonClick: (id) => {
-		let MOD = Game.mods['bulkBuyX'];
-
-		if (id === null || id === undefined) {
+	storeBulkButtonClick: function (id) {
+		if (id === undefined) {
 			// Make the other buttons de-selected
 			if (Game.buyBulk === 1)   l('storeBulk1').classList.remove('selected');
 			if (Game.buyBulk === 10)  l('storeBulk10').classList.remove('selected');
 			if (Game.buyBulk === 100) l('storeBulk100').classList.remove('selected');
 			if (Game.buyBulk === -1)  l('storeBulkMax').classList.remove('selected');
 			// select the X button
-			MOD.btnX.classList.add('selected');
-			MOD.updateBulk();
+			this.btnX.classList.add('selected');
+			this.updateBulk();
 			PlaySound('snd/tick.mp3');
 		} else {
 			// This will only be triggered if one of the normal buy buttons are clicked, so just 
 			// run the normal function and deselect the X button
-			Game.storeBulkButton(id);
-			MOD.btnX.classList.remove('selected');
+			if (Game.mods['bulkBuy50'] !== undefined) {
+				Game.mods['bulkBuy50'].storeBulkButtonClick(id);
+			} else {
+				Game.storeBulkButton(id);
+			}
+			this.btnX.classList.remove('selected');
 		}
 	},
 
-	updateBulk: () => {
-		let MOD = Game.mods['bulkBuyX'];
+	updateBulk: function () {
+		if (!this.btnX.classList.contains('selected')) return;
 
-		if (!MOD.btnX.classList.contains('selected')) return;
-
-		let x = Number.parseInt(MOD.btnX.value);
-		if (MOD.btnX.value === '') {
-			MOD.curVal = '';
+		let x = Number.parseInt(this.btnX.value);
+		if (this.btnX.value === '') {
+			this.curVal = '';
 			Game.buyBulk = 0;
-		} else if (!/\D/.test(MOD.btnX.value) && Number.isInteger(x) && Number.isFinite(x) && x >= 0 && x <= 1000) {
+		} else if (!/\D/.test(this.btnX.value) && Number.isInteger(x) && Number.isFinite(x) && x >= 0 && x <= 1000) {
 			// Falls within parameters
-			MOD.curVal = x;
+			this.curVal = x;
 			Game.buyBulk = x;
 		} else {
 			// Invalid value; fallback to curVal
-			MOD.btnX.value = MOD.curVal;
+			this.btnX.value = this.curVal;
 			return; // do not update store
 		}
 		Game.storeToRefresh = 1;
 	},
 
-	// mod functions
-	init: () => {
-		let MOD = Game.mods['bulkBuyX'];
+	adjustForBulk50: function () {
+		let MOD = this;
+		// Get all the other bulk buttons
+		document.querySelectorAll('.storeBulkAmount').forEach(btn => {
+			// Modify the onclick behavior of the buttons so they select and deselect properly
+			let id;
+			switch (btn.innerHTML.toLowerCase()) {
+				case '1':   id = 2;   btn.style.width = '25px'; return;
+				case '10':  id = 3;   btn.style.width = '35px'; return;
+				case '50':  id = 3.5; btn.style.width = '35px'; break; // Update btn50 onlick only
+				case '100': id = 4;   btn.style.width = '45px'; return;
+				case 'all': id = 5;   btn.style.width = '27px'; return;
+				default: return;
+			}
+			btn.setAttribute('onclick', `Game.mods['${MOD.id}'].storeBulkButtonClick(${id})`);
+		});
+	},
+
+	init: function () {
+		let MOD = this;
 
 		MOD.curVal = 50;
 
@@ -56,34 +71,37 @@ Game.registerMod("bulkBuyX", {
 		let btnX = document.createElement('input');
 		btnX.setAttribute('type', 'text');
 		btnX.setAttribute('id', 'storeBulkX');
-		
+
 		// Get all the other bulk buttons
 		document.querySelectorAll('.storeBulkAmount').forEach(btn => {
 			// Modify the onclick behavior of the buttons so they select and deselect properly
 			let id;
 			switch (btn.innerHTML.toLowerCase()) {
-				case 'buy':
-				case 'sell': return; // We don't want to change the behavior of buy/sell
 				// These ids are taken from the original code
-				case '1':   id = 2; btn.style.width = '42px'; break;
+				case '1':   id = 2; btn.style.width = '32px'; break;
 				case '10':  id = 3; btn.style.width = '42px'; break;
-				case '100': id = 4; btn.style.width = '42px'; break;
-				case 'all': id = 5; btn.style.width = '32px'; break;
+				case '100': id = 4; btn.style.width = '52px'; break;
+				case 'all': id = 5; btn.style.width = '40px'; break;
+				default: return;
 			}
 			btn.setAttribute('onclick', `Game.mods['${MOD.id}'].storeBulkButtonClick(${id})`);
 		});
 
+		if (Game.mods['bulkBuy50'] !== undefined) this.adjustForBulk50();
+
 		btnX.className = 'storePreButton storeBulkAmount';
-		btnX.style.width = '48px';
+		btnX.style.width = '45px';
 		btnX.style.fontFamily = 'Tahoma';
 		btnX.style.fontSize = '14px';
 		btnX.style.fontWeight = 'bold';
 		btnX.style.textAlign = 'center';
 		btnX.style.background = 'none';
 		btnX.style.color = '#fff';
-		btnX.style.height = '7px';
-		btnX.style.margin = '2px 10px 0 10px';
+		btnX.style.height = '5px';
+		btnX.style.margin = '2px 8px 0 8px';
 		btnX.style.outline = 'none';
+		btnX.style.position = 'relative';
+		btnX.style.top = '1px';
 		
 		btnX.value = MOD.curVal;
 
@@ -95,13 +113,11 @@ Game.registerMod("bulkBuyX", {
 
 		MOD.btnX = btnX;
 	},
-	save: () => {
-		let MOD = Game.mods['bulkBuyX'];
-		return MOD.curVal.toString();
+	save: function () {
+		return this.curVal.toString();
 	},
-	load: (str) => {
-		let MOD = Game.mods['bulkBuyX'];
-		MOD.curVal = str;
-		MOD.btnX.value = MOD.curVal;
+	load: function (str) {
+		this.curVal = str;
+		this.btnX.value = this.curVal;
 	}
 });
