e2d63bfe285f679c41487445c2a143b2cf750731
Jasper Andrew <jasper.q.andrew@gmail.com>
2022-06-20 03:03:12 -0400

    moved resource imgs

--- enableRefresh/main.js
+++ enableRefresh/main.js
@@ -3,9 +3,9 @@ Game.registerMod("enableRefresh", {
         AddEvent(window, 'keydown', (e) => {
             Game.Notify(e.key,'',[16,5]);
             if (e.key == '5') {
-                App.reload();
-                window.location.reload();
-                // e.preventDefault();
+                Game.Notify(`Restarting the game!`,'',[0,0], true); //For people interested: Game.Notify(title,desc,pic,quick,noLog) quick = Notification disappears automatically after around a second. noLog = Doesn't display in console
+                Game.toSave = true;
+                Game.toReload = true; //Turns out CC actually saves the game before reloading, it was an oopsie on my side. But now it's fixed
             }
         });
         Game.Notify(`Enable Refresh loaded!`,'',[16,5]);
