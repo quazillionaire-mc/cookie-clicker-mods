db577f5cf17225db3200bab152f7fc3af5bcaf46
Jasper Andrew <jasper.q.andrew@gmail.com>
2022-06-27 23:52:38 -0400

    Move the chip icon to the match other currencies

--- beautifyChips/main.js
+++ beautifyChips/main.js
@@ -3,24 +3,15 @@ Game.registerMod("beautifyChips", {
 		let style = document.createElement("style");
 		// style.appendChild(document.createTextNode(""));
 		document.head.appendChild(style);
-		style.sheet.insertRule('.chipCount {' +
-			'margin-right: 21px; }');
-		style.sheet.insertRule('.chipCount::after {' +
-			'content: "";' +
-			'display: block;' +
-			'position: absolute;' +
-			'right: 2px;' +
-			'top: 8px;' +
-			'background: url(img/heavenlyMoney.png);' +
-			'width: 16px;' +
-			'height: 16px; }');
+		style.sheet.insertRule('.chipCount { color: #fff; }');
+		style.sheet.insertRule('.chipCount::before { top: 1px; }');
 
 		Game.ascendNumber.style.whiteSpace = 'pre';
 		
 		let logic = () => {
 			var ascendNowToGet = Math.floor(Game.HowMuchPrestige(Game.cookiesReset + Game.cookiesEarned)) - Math.floor(Game.HowMuchPrestige(Game.cookiesReset));
 			Game.ascendNumber.innerHTML = 
-				'<span class="chipCount">' + '+' + Beautify(ascendNowToGet).replace(/\.\d\d$/, '') + '</span>';
+				'+<span class="chipCount heavenly price">' + Beautify(ascendNowToGet).replace(/\.\d\d$/, '') + '</span>';
 		};
 
 		Game.registerHook('logic', () => {
